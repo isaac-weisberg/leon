6284589aa34b7b2382938cdd72ab8bf389805500
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-11-18 17:55:25 +0700

    Add harassment results report

--- src/report/report.ts
+++ src/report/report.ts
@@ -1,5 +1,17 @@
 import { HarrassmentResults } from "../harrasser/harasser";
 
 export async function report(results: HarrassmentResults) {
-    
+    return JSON.stringify(
+        {
+            base: results.base,
+            bodyless: results.bodyless.map(results => {
+                const errs = results.errors.length > 0 ? { errors: results.errors.map(error => error.unity()) } : {}
+                return {
+                    route: results.route.unity(),
+                    errorCount: results.errors.length,
+                    ...errs
+                }
+            })
+        },
+        null, 4)
 }
