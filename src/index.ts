8fc252c4e9c642045fbc1ee95d200a68e940b051
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-11-18 18:24:26 +0700

    Add a direct fucking usage case lol to the index lel

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
 import harasser from "./harrasser/harasser";
 import report from "./report/report";
+import View from "./view/View";
 
 export default {
     harrasser: harasser,
     report: report
 }
+
+const vuew: View = {
+    base: new URL('https://dev.fuckingserver/api/v1'),
+    bodyless: [
+        
+    ]
+};
+
+(async () => {
+    const res = await harasser(vuew)
+    const rep = await report(res)
+    process.stdout.write(rep)
+})()
